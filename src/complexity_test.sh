2b65b0551f792a59202cd1fa24da5cd212d7d533
artur <artur11jorge@gmail.com>
2019-12-08 22:24:50 +0000

    progress in the doc

--- src/complexity_test.sh
+++ src/complexity_test.sh
@@ -1,10 +1,10 @@
-how_many_tests=1
-gap_between_tests=1000000
+how_many_tests=100
+gap_between_tests=10000
 for i in `seq 1 $how_many_tests`; do
     echo $i;
     for j in `seq 1 $((i*gap_between_tests))`; do 
-        echo $((-450 + RANDOM % 900)) $((-300 + RANDOM % 600));
+        echo $RANDOM $RANDOM;
     done > "tests/test$i.pts";
-    ./hull < "tests/test$i.pts"; #>> "time.txt";
+    ./hull < "tests/test$i.pts" >> "time.txt";
     sleep 0.01
 done
